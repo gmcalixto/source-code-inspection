c8d29a9ab0589c9e9676e037f9623e48a14fe41b
Your Name <you@example.com>
2024-04-10 16:55:18 -0300

    Atualiza saldo, antes de imprimir

--- Source Code Inspection/src/br/calebe/ticketmachine/core/TicketMachine.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/core/TicketMachine.java
@@ -44,6 +44,7 @@ public class TicketMachine {
         if (saldo < valor) {
             throw new SaldoInsuficienteException();
         }
+        saldo -= valor; /* Atualiza saldo, antes de imprimir */
         String result = "*****************\n";
         result += "*** R$ " + saldo + ",00 ****\n";
         result += "*****************\n";
