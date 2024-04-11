e1d18d4273acc5ead777f4efdf923c6eb9b44965
VITOR AUGUSTO L P DOS SANTOS <unifvisantos@feinet.fei.edu.br>
2024-04-10 22:07:44 -0300

    Indice maior que o tamanho da lista

--- Source Code Inspection/src/br/calebe/ticketmachine/core/Troco.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/core/Troco.java
@@ -69,7 +69,7 @@ class Troco {
         @Override
         public PapelMoeda next() {
             PapelMoeda ret = null;
-            for (int i = 6; i >= 0 && ret != null; i++) {
+            for (int i = 5; i >= 0 && ret != null; i++) { /* limite maximo da lista é 5 */
                 if (troco.papeisMoeda[i] != null) {
                     ret = troco.papeisMoeda[i];
                     troco.papeisMoeda[i] = null;
