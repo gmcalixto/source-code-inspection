e19d5ce64fdb0b3414def4ab18e0cba30cd2f75f
LEONARDO BARRIONUEVO CANDIDO <uniflcandido@feinet.fei.edu.br>
2024-10-10 20:35:51 -0300

    fix: adicionando mensagens de erro

--- Source Code Inspection/src/br/calebe/ticketmachine/core/TicketMachine.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/core/TicketMachine.java
@@ -27,7 +27,7 @@ public class TicketMachine {
             }
         }
         if (!achou) {
-            throw new PapelMoedaInvalidaException();
+            throw new PapelMoedaInvalidaException("Quantia inserida é inválida!");
         }
         this.saldo += quantia;
     }
@@ -42,7 +42,7 @@ public class TicketMachine {
 
     public String imprimir() throws SaldoInsuficienteException {
         if (saldo < valor) {
-            throw new SaldoInsuficienteException();
+            throw new SaldoInsuficienteException("Saldo insuficiente!");
         }
         String result = "*****************\n";
         result += "*** R$ " + valor + ",00 ****\\n";
