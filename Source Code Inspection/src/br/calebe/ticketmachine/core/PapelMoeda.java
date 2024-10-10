ba8f5d47056c35ca0af4b5d902dbcfa40ed98886
Amanda Pollo Sarlo <89867844+amandapollosarlo@users.noreply.github.com>
2024-10-10 20:40:09 -0300

    Update PapelMoeda.java

--- Source Code Inspection/src/br/calebe/ticketmachine/core/PapelMoeda.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/core/PapelMoeda.java
@@ -5,6 +5,11 @@ package br.calebe.ticketmachine.core;
  * @author Calebe de Paula Bianchini
  */
 public class PapelMoeda {
+    if (valor != 2 && valor != 5 && valor != 10 && valor != 20 && valor != 50 && valor != 100) { // Linha 6
+        throw new IllegalArgumentException("Valor de papel-moeda inválido");
+    }
+    this.valor = valor;
+    this.quantidade = quantidade;
 
     protected int valor;
     protected int quantidade;
