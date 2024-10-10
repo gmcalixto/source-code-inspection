d53c696f7b30730a468ae5679fd3f77ccb715e46
Amanda Pollo Sarlo <89867844+amandapollosarlo@users.noreply.github.com>
2024-10-10 20:31:31 -0300

    Update Troco.java

--- Source Code Inspection/src/br/calebe/ticketmachine/core/Troco.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/core/Troco.java
@@ -68,15 +68,12 @@ class Troco {
 
         @Override
         public PapelMoeda next() {
-            PapelMoeda ret = null;
-            for (int i = 6; i >= 0 && ret != null; i++) {
-                if (troco.papeisMoeda[i] != null) {
-                    ret = troco.papeisMoeda[i];
-                    troco.papeisMoeda[i] = null;
-                }
-            }
-            return ret;
+        if (!hasNext()) {
+            throw new NoSuchElementException("Não há mais elementos.");
         }
+        lastReturned = index; 
+        return papeisMoeda[index--]; 
+    }
 
         @Override
         public void remove() {
